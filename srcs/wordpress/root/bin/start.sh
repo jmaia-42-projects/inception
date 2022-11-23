292566ca55e841d54499fedb18fdc10c259cd3e7
Jeremy Maia <jmaia@student.42.fr>
2022-11-23 18:08:53 +0100

    Implement redis

--- srcs/wordpress/root/bin/start.sh
+++ srcs/wordpress/root/bin/start.sh
@@ -3,4 +3,8 @@ cd /srv/
 PAGER=cat wp-cli config create --dbname='wordpress' --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost='mariadb'
 PAGER=cat wp-cli core install --url="$DOMAIN_NAME" --title="$WEBSITE_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_MAIL" --skip-email
 PAGER=cat wp-cli user create "$WP_BASIC_USER" "$WP_BASIC_MAIL" --user_pass="$WP_BASIC_PASS"
+
+PAGER=cat wp-cli config set WP_REDIS_HOST redis
+PAGER=cat wp-cli plugin install --activate redis-cache
+PAGER=cat wp-cli redis enable
 /usr/sbin/php-fpm8 -F --allow-to-run-as-root
