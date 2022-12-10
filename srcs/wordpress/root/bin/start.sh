30e60497ff8646e3c0f450af073983aa3febd055
Jeremy Maia <jmaia@student.42.fr>
2022-12-10 22:16:36 +0100

    Move wordpress to a subfolder

--- srcs/wordpress/root/bin/start.sh
+++ srcs/wordpress/root/bin/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 setfacl -R -m 'u:1000:rwx' /srv
-cd /srv/
+cd /srv/wordpress/
 PAGER=cat wp-cli config create --dbname='wordpress' --dbuser="$MYSQL_USER" --dbpass="$MYSQL_PASSWORD" --dbhost='mariadb'
 PAGER=cat wp-cli core install --url="$DOMAIN_NAME" --title="$WEBSITE_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASS" --admin_email="$WP_ADMIN_MAIL" --skip-email
 PAGER=cat wp-cli user create "$WP_BASIC_USER" "$WP_BASIC_MAIL" --user_pass="$WP_BASIC_PASS"
